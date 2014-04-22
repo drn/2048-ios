17e8f49b4a40b93aa419c7c187ca53b2ff99c8da
darrenli <darren@thanx.com>
2014-04-21 23:59:57 -0700

    Update UI.

--- m2048/Controller/M2ViewController.m
+++ m2048/Controller/M2ViewController.m
@@ -90,7 +90,7 @@
   
   _subtitle.textColor = [GSTATE buttonColor];
   _subtitle.font = [UIFont fontWithName:[GSTATE regularFontName] size:14];
-  _subtitle.text = [NSString stringWithFormat:@"Join the numbers to get to %ld!", target];
+  _subtitle.text = [NSString stringWithFormat:@"Join Popo & Minx to win!"];
   
   _overlay.message.font = [UIFont fontWithName:[GSTATE boldFontName] size:36];
   _overlay.keepPlaying.titleLabel.font = [UIFont fontWithName:[GSTATE boldFontName] size:17];
